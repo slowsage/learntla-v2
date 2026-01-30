f3551120cfc85455a0ce53c7dee4fdf71cc7fb59
slowsage <84777606+slowsage@users.noreply.github.com>
2026-01-30 13:16:38 -0500

    feat: prevent search engine indexing

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,7 @@ html_style = "custom.css"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_extra_path = ['robots.txt']
 html_css = ["custom.css"]
 todo_include_todos = True
 trim_footnote_reference_space = True
